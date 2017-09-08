69cf182a900058baeec128cb453ada19550bd9ea
Ryan Leonard <rhinoisnot@gmail.com>
2017-09-08 16:18:08 -0400

    Added properties for 'beforeEach' and 'afterEach' stages.

--- AVAHook.js
+++ AVAHook.js
@@ -4,10 +4,32 @@
 */
 
 /**
+ * A list of stages for a {@link AVAHook}.  Stages are provided as `{ <method name>: <description> }`.
+ * `<method name>` is the name of a method inside the {@link AVAHook} to run during the stage.
+ * @typedef {Object} StageList
+*/
+
+/**
  * Abstract interface for test helpers.
  * @abstract
 */
 class AVAHook {
+
+  /**
+   * The list of stages to setup, with the description to provide `t.beforeEach()`.
+   * @return {StageList} the list of stages for this test helper that should be run in `beforeEach()`.
+  */
+  static get setup() {
+    return {};
+  }
+
+  /**
+   * The list of stages to cleanup, with the description to provide `t.afterEach()`.
+   * @return {StageList} the list of stages for this test helper that should be run in `afterEach()`.
+  */
+  static get cleanup() {
+    return {};
+  }
 
   /**
    * Create a new test helper that can later be connected to AVA.
